ee9f2f3230f8e1c6ff6792e00b4073653066e2b7
Gustaf Broström <gustaf-96@hotmail.com>
2020-01-06 19:16:09 +0100

    m

--- project3.m
+++ project3.m
@@ -3,10 +3,9 @@ clear
 close all
 load fmri.mat
 
-Xa = [X(:,1:2) sum(X(:,3:end),2)];
 
 col_img = colstack(img);
-G = 4*speye(8874,8874);
+G = 4*eye(8874,8874);
 
 up = -1;
 down = 1;
@@ -15,44 +14,55 @@ right = 0;
 for i = 1:8874
     up = i - 1;
     if mod(i,88) == 0 || i == 1
-        G(i,i) = G(i,i) - 1;
+        G(i,i) = G(i,i) - 0;
     else
         G(i,up) = G(i,up) - 1;
-    end
-
-    
+    end    
     down = i + 1;
     if mod(i, 87) == 0
-        G(i,i) = G(i,i) - 1;
+        G(i,i) = G(i,i) - 0;
     else
         G(i,down) = G(i,down) - 1;
     end
-    
     left = i - 87;
     if i < 88
-        G(i,i) = G(i,i) - 1;
+        G(i,i) = G(i,i) - 0;
     else
         G(i,left) = G(i,left) - 1;
     end
-    
     right = i + 87;
     if i > 8787
-        G(i,i) = G(i,i) - 1;
+        G(i,i) = G(i,i) - 0;
     else
         G(i,right) = G(i,right) - 1;
     end
-    
-    
-
 end
+G = sparse(G);
 
-tau = 1;
-Q = tau * G;
 
-Y = img(:);
+
+
+Xa = [X(:,1:2) sum(X(:,3:end),2)];
+
+Y = colstack(col_img);
 sz = size(img);
-A = kron(Xa, speye(sz(1)*sz(2)));
+Akron = kron(Xa, speye(sz(1)*sz(2)));
 %Gibb loop
+
+A = speye(8874);
+
+%init values
+tq = [0.1 0.1 0.1];
+te = 0.1*ones(size(Y));
+
+for i = 1:1
+    Q_0 = kron(diag(tq), G);
+    p = amd(Q_0);
+    A_p = A(:,p);
+    R = chol(Q_0(p,p));
+    X = R \ randn(size(R,1),1);
+    EX = R \ (R' \ (A_p'*Y));
+end
 
 
 
